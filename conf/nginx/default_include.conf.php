bfe034da8da73715d7c3a3ba88a6cbce6c3e2ee2
Jérémy Gaudin <jeremy@Mac-Studio-de-Jeremy.local>
2024-03-12 10:02:10 +0100

    push

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -6,13 +6,3 @@ location / {
 location ~ ^/(composer\.(json|lock|phar)$|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
 	deny all;
 }
-
-error_page 500 502 503 504 =500 /error_500.json;
-location = /error_500.json {
-	root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
-}
-
-error_page 400 401 402 403 404 =400 /error_400.json;
-location = /error_400.json {
-	root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
-}
